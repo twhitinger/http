1d1e315f1874195cef491f30cf7f1adbaeb9be3b
Trent Whitinger <twhitinger@gmail.com>
2016-06-01 19:43:14 -0600

    put some of the game logic in the server and game class for start_game, and game. need to refactor. all tests still pass

--- lib/game.rb
+++ lib/game.rb
@@ -1,22 +1,20 @@
 class Game
-attr_reader :guess, :correct_number
+  attr_reader :guess, :correct_number, :last_guess
   def initialize
     @count = 0
     @correct_number = generate_random_number
-
   end
 
   def guess_check(last_guess)
-      @count += 1
+    @count += 1
+    last_guess = last_guess.to_i
     if last_guess > correct_number
       "Your guess is too high."
     elsif last_guess < correct_number
       "Your guess is too low."
-    else
-      @correct_number = generate_random_number
-      "Your guess is correct."
+    elsif last_guess == correct_number
+      "Your guess is correct. Cya Nerd"
     end
-
   end
 
   def generate_random_number
--- lib/server.rb
+++ lib/server.rb
@@ -2,9 +2,10 @@ require 'socket'
 require 'pry'
 require './lib/request_parser'
 require './lib/path.rb'
+require "./lib/game"
 
 class Server
-  attr_reader :path, :client
+  attr_reader :path, :client, :request, :game
 
   def start_server
     counter = 0
@@ -12,37 +13,49 @@ class Server
 
     loop do
       @client = tcp_server.accept
-      request = RequestParser.new(server_request)
+      @request = RequestParser.new(server_request)
 
-      thing = client.read(request.accept.to_i) if request.path == "/game" && request.verb == "POST"
-
-      @path = Path.new(request.path,request.response,counter,request,thing = 0)
+      if request.path == "/game" && request.verb == "POST"
+        thing = client.read(request.accept.to_i)
+      else
+        thing = 0
+      end
+      play_game(thing)
+      @path = Path.new(request.path,request.response,counter,request)
       output = "<html><head></head><body><pre>#{path.path_finder}</pre></body></html>"
-
       print_or_shutdown(output)
       counter += 1
       client.close
     end
   end
 
-  def get_guess
-  end
-  def print_or_shutdown(output)
-    headers = ["http/1.1 200 ok",
-          "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-          "server: ruby",
-          "content-type: text/html; charset=iso-8859-1",
-          "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-    client.puts headers
-    client.puts output
-     exit if path.path_finder == path.shutdown
+  def play_game(thing)
+
+    if request.path == "/start_game"
+      @game = Game.new
+    elsif request.verb == "GET"
+      print_or_shutdown("<html><head></head><body><pre>#{game.guess_check(thing)}</pre></body></html>")
+    elsif request.verb == "POST"
+      print_or_shutdown("<html><head></head><body><pre>#{game.guess_check(thing)}</pre></body></html>")
+    end
   end
 
-  def server_request
-    request_lines = []
-    while line = @client.gets and !line.chomp.empty?
-      request_lines << line.chomp
+  def print_or_shutdown(output)
+    headers = ["http/1.1 200 ok",
+      "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+      "server: ruby",
+      "content-type: text/html; charset=iso-8859-1",
+      "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+      client.puts headers
+      client.puts output
+      exit if path.path_finder == path.shutdown
     end
-    request_lines
+
+    def server_request
+      request_lines = []
+      while line = @client.gets and !line.chomp.empty?
+        request_lines << line.chomp
+      end
+      request_lines
+    end
   end
-end
