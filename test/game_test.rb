02691360f8cd8aaf81ebf70c3eb26ccae86805fd
Lane Winham <mlwinham12@gmail.com>
2016-06-02 20:13:10 -0600

    added some bs testing

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,7 +1,31 @@
 require './test/test_helper'
-
+require './lib/game'
 require 'faraday'
 
 
-class PathTest < Minitest::Test
+class GameTest < Minitest::Test
+  attr_reader :game
+  def setup
+    @game ||= Game.new
+  end
+
+  def test_it_can_be_created
+
+    assert_instance_of Game, game
+  end
+
+  def test_it_has_a_count
+
+    assert_equal 0, game.count
+  end
+
+  def test_if_no_guess_last_guess_returns_nil
+
+    assert_equal nil, game.last_guess
+  end
+
+  def test_if_no_guess_returns_nil
+
+    assert_equal nil, game.guess
+  end
 end
